b9965baaa942d95c047a65d86bc23e027f2da8f8
DID0101 <didarpurliyew750@gmail.com>
2023-05-12 15:03:54 +0300

    first commit

---
+++ DataAccess/Abstract/IProduct.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProduct: IEntityRepository<Product>
+    {
+
+       // List<Product> GetAll();
+        //Repesitory Pattern 
+        //Listeleme 
+        //eklem 
+        //Silme 
+        // Guncelleme
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryDal:ICategoryDal
+    {
+        // must be implement interface  but its didnt do that meybe there has problem 
+    }
+}
